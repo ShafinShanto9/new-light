3895c035dcb2ff5aee0400445620696c22bf5997
shafin_shant0 <shafinshanto9@gmail.com>
2022-09-11 15:58:12 +0600

    router config done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import Details from './pages/Details';
+import BlogOparation from './pages/BlogOparation';
+import Authentication from './pages/Authentication';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ToastContainer/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/home' element={<Home/>} />
+        <Route path='/detail/:id' element={<Details/>} />
+        <Route path='/update/:id' element={<BlogOparation/>} />
+        <Route path='/create' element={<BlogOparation/>} />
+        <Route path='/auth' element={<Authentication/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='*' element={<NotFound/>} />
+      </Routes>
     </div>
   );
 }
